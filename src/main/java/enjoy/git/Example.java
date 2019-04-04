faef908b448f5f6dbae9e308b8971b5027362e58
dengwujun <49271963+dengwujun@users.noreply.github.com>
2019-04-04 18:39:09 +0800

    Update Example.java
    999

--- src/main/java/enjoy/git/Example.java
+++ src/main/java/enjoy/git/Example.java
@@ -11,7 +11,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello Git! update  + github";
+        return "Hello Git! update  + github999";
     }
 
     public static void main(String[] args) {
