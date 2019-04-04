5e9582bbba8d0780519d09133eaa37f8b2b91612
dengwujun <992133520@qq.com>
2019-04-04 18:09:59 +0800

    v0.10

--- src/main/java/enjoy/git/Example.java
+++ src/main/java/enjoy/git/Example.java
@@ -11,7 +11,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello Git! -v0.8 -master update --marge合并分支king";
+        return "Hello Git! update  + github";
     }
 
     public static void main(String[] args) {
